9c83b425e914de1f807736ad2c13a51cd62a226f
tsuyuzaki <yudai.tsuyuzaki@gmail.com>
2023-12-04 14:08:21 +0900

    アイテム追加として対応

---
+++ block/src/survey-select/edit-list-block.js
@@ -0,0 +1,117 @@
+import { isEmpty, tap, noop, split, trim } from "lodash";
+import { useRef } from "@wordpress/element";
+import { RichText } from "@wordpress/block-editor";
+
+const setFocus = (wrapper, selector, index, cursorToEnd) => {
+	setTimeout(() => {
+		tap(wrapper.querySelectorAll(selector)[index], (input) => {
+			if (!input) {
+				return;
+			}
+
+			input.focus();
+
+			// 全削除
+			if (document.createRange && cursorToEnd) {
+				const range = document.createRange();
+				range.selectNodeContents(input);
+				range.collapse(false);
+				const selection = document.defaultView.getSelection();
+				selection.removeAllRanges();
+				selection.addRange(range);
+			}
+		});
+	}, 0);
+};
+
+export default function EditListBlock({ attributes, setAttributes }) {
+	const setItems = (items) => setAttributes({ items });
+	const { items, tagType } = attributes;
+
+	const itemsRef = useRef();
+	const changeFocus = (index, cursorToEnd) =>
+		setFocus(itemsRef.current, "[role=textbox]", index, cursorToEnd);
+
+	const handleSingleValue = (index, value) => {
+		const _items = [...items];
+		_items[index] = value;
+
+		setItems(_items);
+		changeFocus(index);
+	};
+
+	const handleMultiValues = (index, array) => {
+		const _items = [...items];
+		const cursorToEnd = array[array.length - 1] !== "";
+
+		if (_items[index]) {
+			_items[index] = array.shift();
+			index++;
+		}
+
+		_items.splice(index, 0, ...array);
+
+		setItems(_items);
+		changeFocus(index + array.length - 1, cursorToEnd);
+	};
+
+	const handleChange = (index) => (value) => {
+		const values = split(value, "\n").filter((op) => op && trim(op) !== "");
+
+		if (!values.length) {
+			return;
+		}
+
+		if (values.length > 1) {
+			handleMultiValues(index, values);
+		} else {
+			handleSingleValue(index, values.pop());
+		}
+	};
+
+	const handleSplit = (index) => (value, isOriginal) => {
+		if (!isOriginal) {
+			return;
+		}
+
+		const splitValue = items[index].slice(value.length);
+
+		if (isEmpty(value) && isEmpty(splitValue)) {
+			return;
+		}
+
+		handleMultiValues(index, [value, splitValue]);
+	};
+
+	const handleDelete = (index) => () => {
+		if (items.length === 1) {
+			return;
+		}
+
+		const _items = [...items];
+		_items.splice(index, 1);
+		setItems(_items);
+		changeFocus(Math.max(index - 1, 0), true);
+	};
+
+	return (
+		<div ref={itemsRef}>
+			{items.map((value, index) => (
+				<div>
+					{"select" !== tagType && <input type={tagType} />}
+					<RichText
+						tagName="label"
+						key={index}
+						value={value}
+						onChange={handleChange(index)}
+						onSplit={handleSplit(index)}
+						onRemove={handleDelete(index)}
+						onReplace={noop}
+						placeholder={"項目追加"}
+						__unstableDisableFormats
+					/>
+				</div>
+			))}
+		</div>
+	);
+}
--- block/src/survey-select/index.js
+++ block/src/survey-select/index.js
@@ -1,12 +1,11 @@
 import { registerBlockType } from "@wordpress/blocks";
 import { __ } from "@wordpress/i18n";
 import { useBlockProps, RichText } from "@wordpress/block-editor";
-import { isEmpty, tap, noop, split, trim } from "lodash";
-import { useEffect, useRef } from "@wordpress/element";
 
 import metadata from "./block.json";
 import "./style.scss";
 import FieldControls from "./field-controls";
+import EditListBlock from "./edit-list-block";
 
 /**
  * アンケートフォームの選択肢
@@ -35,103 +34,14 @@ registerBlockType(metadata.name, {
 			type: "boolean",
 			default: false,
 		},
-		options: {
+		items: {
 			type: "array",
 			default: [""],
 		},
 	},
 
 	edit: ({ attributes, setAttributes }) => {
-		const { options } = attributes;
-
-		const setFocus = (wrapper, selector, index, cursorToEnd) =>
-			setTimeout(() => {
-				tap(wrapper.querySelectorAll(selector)[index], (input) => {
-					if (!input) {
-						return;
-					}
-
-					input.focus();
-
-					// Allows moving the cursor to the end of
-					// 'contenteditable' elements like <RichText />
-					if (document.createRange && cursorToEnd) {
-						const range = document.createRange();
-						range.selectNodeContents(input);
-						range.collapse(false);
-						const selection = document.defaultView.getSelection();
-						selection.removeAllRanges();
-						selection.addRange(range);
-					}
-				});
-			}, 0);
-
 		const blockProps = useBlockProps();
-		const optionsWrapper = useRef();
-		const changeFocus = (index, cursorToEnd) =>
-			setFocus(optionsWrapper.current, "[role=textbox]", index, cursorToEnd);
-		const handleSingleValue = (index, value) => {
-			const _options = [...options];
-
-			_options[index] = value;
-
-			setAttributes({ options: _options });
-			changeFocus(index);
-		};
-
-		const handleMultiValues = (index, array) => {
-			const _options = [...attributes.options];
-			const cursorToEnd = array[array.length - 1] !== "";
-
-			if (_options[index]) {
-				_options[index] = array.shift();
-				index++;
-			}
-
-			_options.splice(index, 0, ...array);
-
-			setAttributes({ options: _options });
-			changeFocus(index + array.length - 1, cursorToEnd);
-		};
-
-		const handleChangeOption = (index) => (value) => {
-			const values = split(value, "\n").filter((op) => op && trim(op) !== "");
-
-			if (!values.length) {
-				return;
-			}
-
-			if (values.length > 1) {
-				handleMultiValues(index, values);
-			} else {
-				handleSingleValue(index, values.pop());
-			}
-		};
-
-		const handleSplitOption = (index) => (value, isOriginal) => {
-			if (!isOriginal) {
-				return;
-			}
-
-			const splitValue = attributes.options[index].slice(value.length);
-
-			if (isEmpty(value) && isEmpty(splitValue)) {
-				return;
-			}
-
-			handleMultiValues(index, [value, splitValue]);
-		};
-
-		const handleDeleteOption = (index) => () => {
-			if (attributes.options.length === 1) {
-				return;
-			}
-
-			const _options = [...attributes.options];
-			_options.splice(index, 1);
-			setAttributes({ options: _options });
-			changeFocus(Math.max(index - 1, 0), true);
-		};
 
 		return (
 			<>
@@ -147,24 +57,10 @@ registerBlockType(metadata.name, {
 						{attributes.required && <span>(必須)</span>}
 					</div>
 
-					<div className="jetpack-field-dropdown__popover" ref={optionsWrapper}>
-						{attributes.options.map((value, index) => (
-							<div>
-								<input type="radio" />
-								<RichText
-									tagName="label"
-									key={index}
-									value={value}
-									onChange={handleChangeOption(index)}
-									onSplit={handleSplitOption(index)}
-									onRemove={handleDeleteOption(index)}
-									onReplace={noop}
-									placeholder={"項目追加"}
-									__unstableDisableFormats
-								/>
-							</div>
-						))}
-					</div>
+					<EditListBlock
+						attributes={attributes}
+						setAttributes={setAttributes}
+					/>
 				</div>
 				<FieldControls attributes={attributes} setAttributes={setAttributes} />
 			</>
@@ -172,26 +68,28 @@ registerBlockType(metadata.name, {
 	},
 	save: ({ attributes }) => {
 		const blockProps = useBlockProps.save();
-		const name = attributes.content.replace(/(<([^>]+)>)/gi, "");
-		const required = attributes.required;
+		const { content, items, required, tagType } = attributes;
+		const name = content.replace(/(<([^>]+)>)/gi, "");
 
 		return (
 			<div {...blockProps}>
 				<div style={{ alignItems: "baseline" }}>
-					<RichText.Content tagName="label" value={attributes.content} />{" "}
+					<RichText.Content tagName="label" value={content} />{" "}
 					{required && <span>(必須)</span>}
 				</div>
 
-				<div className="jetpack-field-dropdown__popover">
-					{attributes.options.map((value, index) => (
+				<div>
+					{items.map((value, index) => (
 						<div>
-							<input
-								type="radio"
-								id={value}
-								name={name}
-								value={value}
-								required={required}
-							/>
+							{"select" !== tagType && (
+								<input
+									type={tagType}
+									name={name}
+									id={value}
+									value={value}
+									required={required}
+								/>
+							)}
 							<RichText.Content
 								tagName="label"
 								key={index}
