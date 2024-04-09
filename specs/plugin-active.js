79ed6f5968f991f69ea2baefdb75eb9e185a5ada
tsuyuzaki <yudai.tsuyuzaki@gmail.com>
2024-04-09 18:38:15 +0900

    format

--- specs/plugin-active.js
+++ specs/plugin-active.js
@@ -1,17 +1,17 @@
-import { loginUser, visitAdminPage } from "@wordpress/e2e-test-utils";
+import { loginUser, visitAdminPage } from '@wordpress/e2e-test-utils';
 
-describe("plugin active", () => {
-	it("verifies the plugin is active", async () => {
+describe( 'plugin active', () => {
+	it( 'verifies the plugin is active', async () => {
 		await loginUser();
-		await visitAdminPage("plugins.php");
+		await visitAdminPage( 'plugins.php' );
 
-		const pluginSlug = "beastfeedbacks";
+		const pluginSlug = 'beastfeedbacks';
 		const activePlugin = await page.$x(
 			'//tr[contains(@class, "active") and contains(@data-slug, "' +
 				pluginSlug +
-				'")]',
+				'")]'
 		);
 
-		expect(activePlugin?.length).toBe(1);
-	});
-});
+		expect( activePlugin?.length ).toBe( 1 );
+	} );
+} );
